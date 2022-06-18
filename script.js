7d855f8ae19d9177c1f163e24962f4d99637132e
Esdreelxs <105323148+Esdreelxs@users.noreply.github.com>
2022-06-17 22:16:54 -0300

    Agrego la base del Script precio dolar

---
+++ script.js
@@ -0,0 +1,27 @@
+const cargarDolares = async()=>{
+    let dolares='';
+    try{
+        const respuesta = await fetch('https://www.dolarsi.com/api/api.php?type=valoresprincipales');
+        console.log(respuesta);
+        const datos = await respuesta.json();
+        console.log(datos);
+        let peliculas='';
+        datos.forEach(casa => {
+            peliculas += `<h1>${casa.casa.nombre} compra: $${casa.casa.compra} venta: $${casa.casa.compra} variacion:${casa.casa.variacion}</h1>` ;
+
+
+
+        });
+    document.getElementById('contenedor').innerHTML=peliculas;
+        }
+    
+    
+
+    catch(error){
+        console.log(error);
+    }
+}
+
+
+
+cargarDolares();
