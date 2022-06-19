235954327e2f9a75f2b8b868f55df5df34c65f4c
maleaznarez <105256723+maleaznarez@users.noreply.github.com>
2022-06-19 17:57:15 -0300

    Update dolar.js

--- JScript/dolar.js
+++ JScript/dolar.js
@@ -7,19 +7,24 @@ const cargarDolares = async()=>{
         console.log(datos);
     
         // Dolar oficial
-        const box1=`<center class="titulo"> ${datos[0].casa.nombre}</center>  Compra: $${datos[0].casa.compra} <br>Venta: $${datos[0].casa.venta}<br>`;
+        const box1=`<center class="titulo"> ${datos[0].casa.nombre}</center>  Compra: $${datos[0].casa.compra} <br>Venta: $${datos[0].casa.venta} 
+        <br>Variacion: ${datos[0].casa.variacion}%`;
         document.getElementById('box1').innerHTML=box1;
  
-        const box2=`<center class="titulo"> ${datos[1].casa.nombre}</center>  Compra: $${datos[1].casa.compra} <br>Venta: $${datos[1].casa.venta}<br>`;
+        const box2=`<center class="titulo"> ${datos[1].casa.nombre}</center>  Compra: $${datos[1].casa.compra} <br>Venta: $${datos[1].casa.venta} 
+        <br>Variacion: ${datos[1].casa.variacion}%`;
         document.getElementById('box2').innerHTML=box2;
         //Dolar 
-        const box3=` <center class="titulo"> ${datos[3].casa.nombre}</center>  Compra: $${datos[3].casa.compra} <br>Venta: $${datos[3].casa.venta}<br>`;
+        const box3=` <center class="titulo"> ${datos[3].casa.nombre}</center>  Compra: $${datos[3].casa.compra} <br>Venta: $${datos[3].casa.venta}
+        <br>Variacion: ${datos[3].casa.variacion}%`;
         document.getElementById('box3').innerHTML=box3;
         //Dolar 
-        const box4=`<center class="titulo"> ${datos[4].casa.nombre}</center>  Compra: $${datos[4].casa.compra} <br>Venta: $${datos[4].casa.venta}<br><`;
+        const box4=`<center class="titulo"> ${datos[4].casa.nombre}</center>  Compra: $${datos[4].casa.compra} <br>Venta: $${datos[4].casa.venta}
+        <br>Variacion: ${datos[4].casa.variacion}%`;
         document.getElementById('box4').innerHTML=box4;
         //Dolar 
-        const box5=`<center class="titulo"> ${datos[6].casa.nombre}</center>  Compra: $${datos[6].casa.compra} <br>Venta: $${datos[6].casa.venta}<br>`;
+        const box5=`<center class="titulo"> ${datos[6].casa.nombre}</center>  Compra: $${datos[6].casa.compra} <br>Venta: $${datos[6].casa.venta}
+        <br>Variacion: ${datos[6].casa.variacion}%`;
         document.getElementById('box5').innerHTML=box5;
         //Dolar 
         const box6=`<center class="titulo"> ${datos[7].casa.nombre}</center>  Compra: $${datos[7].casa.compra} <br>Venta: $${datos[7].casa.venta}<br>`;
